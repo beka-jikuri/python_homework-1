a65a2fbc7a6e740fc44545d3702dcc417b12d67d
KK1723 <Beka.Jikuri@gmail.com>
2023-03-30 18:39:33 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+# Task N1
+
+name = "Beka"
+age = 32
+
+print("name:", name)
+print("age:", age)
+
+
+# Task N2 ---------------------
+
+# დავალებაში ვერ მივხვდი უბრალოდ ლოუერ ქეისებით დამეწერა ქვეყნის სახელი თუ lower მეთოდი უნდა გამომეყენებინა, ამიტომ უფრო რთულად დავწერე.
+# ფაქტიურად კაპიტალიზაცია გავუკეთე ცვლადს, მერე ლოუერის მეთოდით დავაპატარავე და მერე თაითლით გავზარდე :) 
+
+population = 3.5
+country_init = "Georgia"
+country = country_init.lower()
+
+print(f"{country.title()}'s population is {population} million")
+
+
+# ეს იქნებოდა მეორე ხერხი ალბათ.
+
+country_2 = "georgia"
+population = 3.5
+
+print(f"{country_2.title()}'s population is {population} million")
